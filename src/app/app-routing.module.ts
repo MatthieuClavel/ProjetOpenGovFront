5cd188fbd3847de916cdaa535906d5733c0272e2
MatthieuClavel <56390967+MatthieuClavel@users.noreply.github.com>
2019-10-22 14:28:43 +0200

    Commit for routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,33 @@
+import { SruveyCreateComponent } from './survey/sruvey-create/sruvey-create.component';
+import { SurveyListComponent } from './survey/survey-list/survey-list.component';
+import { SurveyComponent } from './survey/survey.component';
+import { ProposalListComponent } from './proposal/proposal-list/proposal-list.component';
+import { ProposalCreateComponent } from './proposal/proposal-create/proposal-create.component';
+import { ProposalComponent } from './proposal/proposal.component';
+import { EnteraccountComponent } from './authentification/enteraccount/enteraccount.component';
+import { AuthentificationComponent } from './authentification/authentification.component';
+import { CreateaccountComponent } from './authentification/createaccount/createaccount.component';
+import { AccueilComponent } from './accueil/accueil.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'accueil', component: AccueilComponent },
+  { path: 'authentification', component: AuthentificationComponent, children : [
+      { path: 'createAccount', component: CreateaccountComponent },
+      { path: 'enterAccount', component: EnteraccountComponent }
+    ]},
+  { path: 'proposal', component: ProposalComponent, children : [
+      { path: 'createProposal', component: ProposalCreateComponent },
+      { path: 'listProposal', component: ProposalListComponent }
+    ]},
+  { path: 'survey', component: SurveyComponent, children : [
+      { path: 'createSurvey', component: SruveyCreateComponent },
+      { path: 'listSurvey', component: SurveyListComponent }
+    ]},
+  { path: '', redirectTo: '/accueil', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
